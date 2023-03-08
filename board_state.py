766e08a81ff62688f3a4ef6320d7df340654b9d1
thobyas <thobyascastano@gmail.com>
2023-03-08 17:37:18 -0500

    modifique nombres y empece una prueba para pasar al siguiente estado del board

--- board_state.py
+++ board_state.py
@@ -8,12 +8,12 @@ import random
 
 def random_state(b_width = int, b_height = int):
     board = np.random.randint(0,2,size=(b_height,b_width))
-    new_board = pretty_print(board)
+    new_board = render(board)
     return new_board
     
 
-def pretty_print(board):
-    new_board = []
+def render(board):
+    render_board = []
     rows = np.shape(board)[0]
     columns = np.shape(board)[1]
     for i in range(rows):
@@ -23,10 +23,18 @@ def pretty_print(board):
                 rows_val.append(" ")
             else:
                 rows_val.append("#")
-        new_board.append(rows_val) 
+        render_board.append(rows_val) 
 
-    return new_board    
+    return render_board    
              
+def next_board_state(inicial_board):
+    next_row =[]
+    rows = np.shape(inicial_board)[0]
+    columns = np.shape(inicial_board)[1]
+    
+    test_board = np.append(np.zeros(rows, dtype= int))
+        
+            
 
 
 
